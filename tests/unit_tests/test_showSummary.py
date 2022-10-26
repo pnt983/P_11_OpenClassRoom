228bd46946d6afe68e5e54f53f2e66ae84873336
pnt983 <82818082+pnt983@users.noreply.github.com>
2022-10-26 11:35:23 +0200

    Modification sur les tests showSummary

--- tests/unit_tests/test_showSummary.py
+++ tests/unit_tests/test_showSummary.py
@@ -20,24 +20,27 @@ def test_good_email(clubs_fixture):
     club = get_club(clubs_fixture[0]['email'])
     assert club['name'] == 'Simply Lift'
 
-def test_showSummary(client, monkeypatch, competitions_fixture):
+def test_showSummary(client):
     """
     Given good email
     When showSummary is calling
     Then summary page display
     """
-    def mock_get_club(email):
-        return {
-            "name":"Simply Lift",
-            "email":"john@simplylift.co",
-            "points":"13"
-        }
-    
-    monkeypatch.setattr(server, 'get_club', mock_get_club)
-    monkeypatch.setattr(server, 'competitions', competitions_fixture)
-
     response = client.post('/showSummary', data={'email': 'john@simplylift.co'})
     data = response.data.decode()
 
     assert response.status_code == 200
-    assert data.find('<h2>Welcome, john@simplylift.co <h2>')
+    assert data.find('Welcome, john@simplylift.co') != -1
+
+def test_showSummary_bad_email(client):
+    """
+    Given bad email
+    When showSummary is calling
+    Then redirect index page display
+    """
+    response = client.post('/showSummary', data = {'email' : 'bad@email.com'})
+    data = response.data.decode()
+
+    assert response.status_code == 302
+    assert data.find('Redirecting') != -1
+    
